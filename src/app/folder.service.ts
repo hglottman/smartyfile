6eb6579444054ea794bbdd45992fe02be5609ef0
Your Namehglottman <hglottman@gmail.com>
2018-06-24 18:14:50 +0300

    start service

--- src/app/folder.service.ts
+++ src/app/folder.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject, Observable, Subscriber } from 'rxjs';
+import { File } from './file';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FolderService {
 
-  constructor() { }
+  // allFolders: Array<Folder>;
+  // allFoldersSubject: Subject<Folder[]> = new Subject<Folder[]>();
+  // allFoldersObservable: Observable<Folder[]>;
+
+  allFiles: Array<File>;
+  allFilesSubject: Subject<File[]> = new Subject<File[]>();
+  allFilesObservable: Observable<File[]>;
+
+  constructor(private http: HttpClient) {
+    // this.allFoldersObservable = this.allFoldersSubject.asObservable();
+    this.allFilesObservable = this.allFilesSubject.asObservable();
+
+  }
+
 }
