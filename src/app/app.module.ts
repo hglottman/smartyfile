1417869a1097305dac9a81546a12ec8ef39e4b31
Talfaran <tal1faran@gmail.com>
2018-06-27 14:19:44 +0300

    date woeks with dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,4 @@
-import 
-
-
-{ BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {Component} from '@angular/core';
@@ -16,10 +13,9 @@ import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-
 import {MatSelectModule} from '@angular/material/select';
 import {MatIconModule} from '@angular/material/icon';
-
+import { MatNativeDateModule} from '@angular/material';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -69,7 +65,8 @@ import { HeaderComponent } from './header/header.component';
     MatCardModule,
     AppRoutingModule,
     MatIconModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatNativeDateModule
     
   ],
 
